196b50cb9f7de9e67d381caabeb584063017723c
Caio Lins <caiohrick@outlook.com>
2024-10-13 14:00:56 -0300

    Add ApiService class and implement buscaEnderecoPelo method

--- 7daysofcode-java/src/App.java
+++ 7daysofcode-java/src/App.java
@@ -1,5 +1,7 @@
+import service.ApiService;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(ApiService.buscaEnderecoPelo("https://viacep.com.br/ws/", "01001000", "json"));
     }
 }
---
+++ 7daysofcode-java/src/enumerator/RequestMethod.java
@@ -0,0 +1,5 @@
+package enumerator;
+
+public enum RequestMethod {
+    GET, POST, HEAD, OPTIONS, PUT, DELETE, TRACE;
+}
